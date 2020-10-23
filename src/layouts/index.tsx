625b7e845ee30b4232277b8819bfde3d690841be
hocgin <hocgin@gmail.com>
2020-10-23 23:57:49 +0800

    Update

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import styles from './index.less';
+import GinFooter from '@hocgin/gin-footer';
+import GinEggs from '@hocgin/gin-eggs';
 
 const BasicLayout: React.FC = props => {
   return (
     <div className={styles.container}>
+      <GinEggs/>
       {props.children}
+      <GinFooter/>
     </div>
   );
 };
